31d5113c425265224e34858f167621f4f9fe0bc8
Willian Sales <willian.o.sales@hotmail.com>
2018-03-28 17:29:13 -0300

    Método de teste retirado.

--- INSSBOT/INSSBOT/INSSBOT.Services.ConsoleApp/Program.cs
+++ INSSBOT/INSSBOT/INSSBOT.Services.ConsoleApp/Program.cs
@@ -14,29 +14,14 @@ namespace INSSBOT.Services.ConsoleApp
             //http://localhost:12345
             using (WebApp.Start<Startup>("http://localhost:80"))
             {
-                // Register WebHook
-                // You should replace {YourHostname} with your Internet accessible hosname
                 Bot.Api.SetWebhookAsync("https://26fda9b6.ngrok.io").Wait();
 
                 Console.WriteLine("Server Started");
 
-                // Stop Server after <Enter>
                 Console.ReadLine();
 
-                // Unregister WebHook
                 Bot.Api.DeleteWebhookAsync().Wait();
             }
-
-
-            teste();
-
-        }
-
-        private async void teste ()
-        {
-            //   var botClient = new ]Bot.Api.TelegramBotClient("your API access Token");
-            var me = await Bot.Api.GetMeAsync();
-            System.Console.WriteLine($"Hello! My name is {me.FirstName}");
         }
     }
 }
