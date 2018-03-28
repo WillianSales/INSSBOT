0f3d814c6c4afe4f07f31ff2eb0f63307fce58e0
Willian Sales <willian.o.sales@hotmail.com>
2018-03-28 17:23:35 -0300

    Criação do projeto ConsoleApp, conexão com a API do Telegram Bot.

---
+++ INSSBOT/INSSBOT/INSSBOT.Services.ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSSBOT.Services.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //http://localhost:12345
+            using (WebApp.Start<Startup>("http://localhost:80"))
+            {
+                // Register WebHook
+                // You should replace {YourHostname} with your Internet accessible hosname
+                Bot.Api.SetWebhookAsync("https://26fda9b6.ngrok.io").Wait();
+
+                Console.WriteLine("Server Started");
+
+                // Stop Server after <Enter>
+                Console.ReadLine();
+
+                // Unregister WebHook
+                Bot.Api.DeleteWebhookAsync().Wait();
+            }
+
+
+            teste();
+
+        }
+
+        private async void teste ()
+        {
+            //   var botClient = new ]Bot.Api.TelegramBotClient("your API access Token");
+            var me = await Bot.Api.GetMeAsync();
+            System.Console.WriteLine($"Hello! My name is {me.FirstName}");
+        }
+    }
+}
---
+++ INSSBOT/INSSBOT/INSSBOT.Services.ConsoleApp/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using System.Web.Http;
+
+namespace INSSBOT.Services.ConsoleApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var configuration = new HttpConfiguration();
+            
+            configuration.Routes.MapHttpRoute("WebHook", "{controller}");
+
+            app.UseWebApi(configuration);
+        }
+    }
+}
